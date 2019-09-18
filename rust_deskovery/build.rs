88c40041c5b7217d57cf63234087e70e3271220e
Kirill Bulatov <mail4score@gmail.com>
2019-09-18 15:31:03 +0300

    Better header generation by bindgen (using libc stubs for types)

--- rust_deskovery/build.rs
+++ rust_deskovery/build.rs
@@ -4,26 +4,21 @@ use std::path::PathBuf;
 fn generate_rust_bindings(out_path: PathBuf) {
     let result_path = out_path.join("descovery_bindings.rs");
 
-    // TODO uncomment?
-    //    if result_path.is_file() {
-    //        return;
-    //    }
+    if result_path.is_file() {
+        // TODO uncomment
+        //        return;
+    }
+
+    let includes = ["/usr/local/lib/gcc/arm-none-eabi/7.3.1/include"];
 
     let builder = bindgen::Builder::default()
         .header("../Core/Inc/rust_header.h")
         .use_core()
-        // without this it generates `pub type __uint32_t = ::std::os::raw::c_uint;`
-        .whitelist_recursively(false)
-//        .whitelist_var("huart2")
-//        .whitelist_var("HAL_LD2_GPIO_Port")
-//        .whitelist_var("HAL_LD2_Pin")
-        // todo this is a copy-paste from makefile
-//        .clang_arg("-DUSE_HAL_DRIVER")
-//        .clang_arg("-DSTM32F446xx")
-//        .clang_arg("--target=thumbv7em-none-eabi")
-//        .clang_arg("--verbose")
-//        .clang_arg("-nostdinc")
-        ;
+        .ctypes_prefix("crate::libc")
+        .clang_args(includes.iter().map(|include| format!("-I{}", include)))
+        .clang_arg("--target=thumbv7em-none-eabi")
+        .clang_arg("--verbose")
+        .clang_arg("-nostdinc");
 
     let bindings = builder.generate().expect("Unable to generate bindings");
 
