470f4f26e41295916053a1746562c18484beb3ed
Kirill Bulatov <mail4score@gmail.com>
2019-09-18 16:38:40 +0300

    Remove warnings caused by C names

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 use core::panic::PanicInfo;
 
 // TODO this is fugly
@@ -9,8 +9,6 @@ pub mod libc {
     pub type c_uchar = u8;
     pub type c_char = i8;
 }
-
-//include!(concat!(env!("OUT_DIR"), "/descovery_bindings.rs"));
 include!("descovery_bindings.rs");
 
 #[panic_handler]
@@ -24,12 +22,11 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-//    let s = "Hello, Embedded World";
+    //    let s = "Hello, Embedded World";
 
     unsafe {
-//        outputStr(s.as_ptr(), s.len());
+        //        outputStr(s.as_ptr(), s.len());
         ledControl(true);
         ledControl(false);
-
     }
 }
