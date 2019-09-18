6ca7ae222b17c385d5235672e7dbac4f955f2aa7
Kirill Bulatov <mail4score@gmail.com>
2019-09-18 15:31:03 +0300

    Better header generation by bindgen (using libc stubs for types)

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -2,11 +2,21 @@
 
 use core::panic::PanicInfo;
 
+// TODO this is fugly
+pub mod libc {
+    pub type c_int = i32;
+    pub type c_long = i64;
+    pub type c_uchar = u8;
+    pub type c_char = i8;
+}
+
+include!(concat!(env!("OUT_DIR"), "/descovery_bindings.rs"));
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-//    let mut host_stderr = HStderr::new();
+    //    let mut host_stderr = HStderr::new();
 
     // logs "panicked at '$reason', src/main.rs:27:4" to the host stderr
-//    writeln!(host_stderr, "{}", info).ok();
+    //    writeln!(host_stderr, "{}", info).ok();
     loop {}
 }
