f131beecc658e215302ad62fa3112c88893f74c7
Kirill Bulatov <mail4score@gmail.com>
2019-09-18 14:37:28 +0300

    build.rs + bindgen draft

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -1,5 +1,12 @@
 #![no_std]
 
-fn main() {
-    println!("Hello, world!");
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+//    let mut host_stderr = HStderr::new();
+
+    // logs "panicked at '$reason', src/main.rs:27:4" to the host stderr
+//    writeln!(host_stderr, "{}", info).ok();
+    loop {}
 }
