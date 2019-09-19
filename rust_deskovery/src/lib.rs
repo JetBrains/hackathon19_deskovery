66d36bee81bd510d6dc4ebb99a66bca26fea1d72
Artem Mukhin <Artem.Mukhin@jetbrains.com>
2019-09-19 15:03:13 +0300

    [odometry]: Move to separate crate; add tests; fix position computing

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -3,12 +3,11 @@
 #![feature(core_intrinsics)]
 
 mod compat;
-mod odometry;
 mod generated_images;
 
-use crate::odometry::{OdometryComputer, Position};
+use odometry::{OdometryComputer, Position};
 use core::f64::consts::PI;
-use compat::{display_text_xy, debug_print, display_text, PRX_BR, PRX_BL, PRX_FR, PRX_FL,robot_idle};
+use compat::{display_text_xy, debug_print, display_text, PRX_BR, PRX_BL, PRX_FR, PRX_FL, robot_idle};
 use compat::{
     delay_ms, display_bg_control, led_control, left_ticks, prxData, radar_range, right_ticks,
     LCD5110_clear, LCD5110_set_XY, LCD5110_write_char, LCD5110_write_pict, /*deskovery_motor*/
@@ -56,16 +55,7 @@ fn alarm_char(alarm_idx: u32) -> u8 {
 pub extern "C" fn rust_main() {
     let mut brightness: i32 = 0;
 
-    let mut position = Position {
-        x: 0.0,
-        y: 0.0,
-        theta: 0.0,
-    };
-    let mut odo_computer = OdometryComputer {
-        position: &mut position,
-        old_left_ticks: 0,
-        old_right_ticks: 0,
-    };
+    let mut odo_computer = OdometryComputer::new();
 
     loop {
         robot_idle();
@@ -92,9 +82,10 @@ pub extern "C" fn rust_main() {
             LCD5110_write_char(alarm_char(PRX_FL));
             odo_computer.update(left_ticks(), right_ticks());
         }
-        output_data_line(0, 3, "X: ", || odo_computer.position.x as i32);
-        output_data_line(0, 4, "Y: ", || odo_computer.position.y as i32);
-        output_data_line(0, 5, "T: ", || (odo_computer.position.theta / PI * 180.0) as i32);
+        let position = odo_computer.position();
+        output_data_line(0, 3, "X: ", || position.x as i32);
+        output_data_line(0, 4, "Y: ", || position.y as i32);
+        output_data_line(0, 5, "T: ", || (position.theta / PI * 180.0) as i32);
 
 //            deskovery_motor(400, 400, false);
         //todo test odometry
