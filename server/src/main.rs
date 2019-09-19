217499ff12d67cd77da2ef2ee530b9bde5a363e9
Kirill Bulatov <mail4score@gmail.com>
2019-09-19 15:04:30 +0300

    Use json in rocker responses and requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,30 +1,33 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use std::io::{Cursor, Read};
 use std::sync::Mutex;
-use rocket_contrib::json::Json;
-use serde::{Deserialize};
-use rocket::{Request, Response, Outcome::*};
-use rocket::data::{FromData, Outcome, Transform, Transformed};
-use rocket::{State, Data};
-use rocket::http::{ContentType, Status};
 
+use rocket::data::{FromDataSimple, Outcome};
+use rocket::http::{ContentType, Status};
+use rocket::{Data, State};
+use rocket::{Outcome::*, Request, Response};
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
 
 struct MyData {
-    d: Mutex<_MyData>
+    d: Mutex<_MyData>,
 }
 
 impl MyData {
     fn new() -> Self {
-        MyData { d: Mutex::new(_MyData::new()) }
+        MyData {
+            d: Mutex::new(_MyData::new()),
+        }
     }
 }
 
 struct _MyData {
     controller: ControllerData,
-    deskovery: Vec<DeskoveryData>
+    deskovery: Vec<DeskoveryData>,
 }
 
 impl _MyData {
@@ -36,67 +39,54 @@ impl _MyData {
     }
 }
 
-struct ControllerData { x: i16, y: i16 }
+#[derive(Serialize, Deserialize)]
+struct ControllerData {
+    x: i16,
+    y: i16,
+}
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Default)]
 struct DeskoveryData {
     x: f64,
     y: f64,
     theta: f64,
+    proximity_sensor_1: bool,
+    proximity_sensor_2: bool,
+    proximity_sensor_3: bool,
+    proximity_sensor_4: bool,
+    distance_to_obstacle: u32,
 }
 
-impl DeskoveryData {
-    fn new() -> DeskoveryData {
-        DeskoveryData { x: 0.0, y: 0.0, theta: 0.0 }
-    }
-}
-
-const DESKOVERY_DATA_LIMIT: u64 = 256;
-
-impl<'a> FromData<'a> for DeskoveryData {
+impl FromDataSimple for DeskoveryData {
     type Error = ();
-    type Owned = String;
-    type Borrowed = str;
 
-    fn transform(request: &Request, data: Data) -> Transform<Outcome<Self::Owned, Self::Error>> {
-        let mut stream = data.open().take(DESKOVERY_DATA_LIMIT);
-        let mut string = String::with_capacity((DESKOVERY_DATA_LIMIT / 2) as usize);
-        let outcome: Outcome<Self::Owned, Self::Error> = match stream.read_to_string(&mut string) {
-            Ok(_) => Success(string),
-            Err(e) => Failure((Status::InternalServerError, ()))
-        };
-
-        Transform::Borrowed(outcome)
-    }
-
-    fn from_data(request: &Request, outcome: Transformed<'a, Self>) ->  Outcome<Self, Self::Error>  {
-        let string = outcome.borrowed()?;
-
-        let splits: Vec<&str> = string.split(" ").collect();
-        if splits.len() != 3 || splits.iter().any(|s| s.is_empty()) {
-            return Failure((Status::UnprocessableEntity, ()));
+    fn from_data(_: &Request, data: Data) -> Outcome<Self, Self::Error> {
+        let mut data_vec = Vec::new();
+        if let Err(_) = data.open().read_to_end(&mut data_vec) {
+            return Failure((Status::InternalServerError, ()));
         }
-
-        Success(DeskoveryData {
-            x: splits[0].parse::<f64>().unwrap(),
-            y: splits[1].parse::<f64>().unwrap(),
-            theta: splits[2].parse::<f64>().unwrap()
-        })
+        if let Ok(data) = serde_json::from_slice(&data_vec) {
+            Success(data)
+        } else {
+            Failure((Status::UnprocessableEntity, ()))
+        }
     }
 }
-
 
 #[get("/")]
 fn index() -> Response<'static> {
     let bdy = include_str!("index.html").to_string();
-    Response::build().header(ContentType::HTML).sized_body(Cursor::new(bdy)).finalize()
+    Response::build()
+        .header(ContentType::HTML)
+        .sized_body(Cursor::new(bdy))
+        .finalize()
 }
 
 #[post("/poll", format = "json", data = "<deskovery_data>")]
 fn poll(data: State<MyData>, deskovery_data: Json<DeskoveryData>) -> String {
     let mut d = data.d.lock().unwrap();
     d.deskovery.push(deskovery_data.0);
-    format!("x: {}; y: {}", d.controller.x, d.controller.y)
+    serde_json::to_string(&d.controller).unwrap()
 }
 
 #[get("/push?<x>&<y>")]
@@ -110,10 +100,13 @@ fn push(data: State<MyData>, x: String, y: String) {
             d.controller.x = x;
             d.controller.y = y;
         }
-        _ => println!("Invalid input!")
+        _ => println!("Invalid input!"),
     }
 }
 
 fn main() {
-    rocket::ignite().manage(MyData::new()).mount("/", routes![index, poll, push]).launch();
+    rocket::ignite()
+        .manage(MyData::new())
+        .mount("/", routes![index, poll, push])
+        .launch();
 }
