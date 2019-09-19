276166dee38451cf25b04ad814f1466616a44d46
ortem <ortem00@gmail.com>
2019-09-19 11:09:24 +0300

    Odometry computing: Extract to separate module; use ticks instead of cm

--- rust_deskovery/build.rs
+++ rust_deskovery/build.rs
@@ -5,7 +5,11 @@ fn generate_rust_bindings(out_path: PathBuf) {
     //let result_path = out_path.join("descovery_bindings.rs");
     let result_path = PathBuf::from("src").join("descovery_bindings.rs");
 
-    let includes = ["/usr/local/lib/gcc/arm-none-eabi/7.3.1/include","C:\\tools\\GNU Tools Arm Embedded\\7 2018-q2-update\\lib\\gcc\\arm-none-eabi\\7.3.1\\include"];
+    let includes = [
+        "/usr/local/lib/gcc/arm-none-eabi/7.3.1/include",
+        "C:\\tools\\GNU Tools Arm Embedded\\7 2018-q2-update\\lib\\gcc\\arm-none-eabi\\7.3.1\\include",
+        "/usr/local/Cellar/arm-none-eabi-gcc/8-2018-q4-major/gcc/lib/gcc/arm-none-eabi/8.2.1/include"
+    ];
 
     let builder = bindgen::Builder::default()
         .header("../Core/Inc/rust_header.h")
