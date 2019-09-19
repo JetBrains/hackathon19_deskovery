b3bc0b8aebabb9ef553cca375ba54965d542a297
Kirill Bulatov <mail4score@gmail.com>
2019-09-19 15:04:30 +0300

    Use json in rocker responses and requests

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -2,11 +2,24 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct DeskoveryData {
+    x: f64,
+    y: f64,
+    theta: f64,
+    proximity_sensor_1: bool,
+    proximity_sensor_2: bool,
+    proximity_sensor_3: bool,
+    proximity_sensor_4: bool,
+    distance_to_obstacle: u32,
+}
 
+#[derive(Serialize, Deserialize)]
+pub enum RequestType {
+    Move
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct ServerData {
     x: i32,
-    y: i32
+    y: i32,
+    theta: f64,
 }
