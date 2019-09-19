e210146d56651be92908d488a15aaccbf8759cdc
ortem <ortem00@gmail.com>
2019-09-19 11:09:24 +0300

    Odometry computing: Extract to separate module; use ticks instead of cm

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -2,13 +2,11 @@
 #![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 #![feature(core_intrinsics)]
 
-use core::intrinsics::{cosf64, sinf64};
+mod odometry;
+
 use core::panic::PanicInfo;
+use crate::odometry::{Position, OdometryComputer};
 
-const WHEEL_RADIUS_CM: f64 = 3.5;
-const WHEEL_BASE_CM: f64 = 14.0;
-
-const PI: f64 = core::f64::consts::PI;
 
 // TODO this is fugly
 pub mod libc {
@@ -42,26 +40,29 @@ fn display_text(x: u8, y: u8, s: &str) {
 fn output_data_line<F>(y: u8, label: &str, dataGetter: F) where F: FnOnce() -> i32 {
     display_text(0, y, label);
     let mut buf: [u8; 10] = [0; 10];
-    let mut index = buf.len();
+    let mut index = buf.len() - 1;
     let mut val = dataGetter();
     let sign = val < 0;
     if sign {
         val = -val;
     }
     loop {
-        index -= 1;
         buf[index] = (val % 10 + 48) as u8;
+        index -= 1;
         val = val / 10;
         if val == 0 {
             break;
         }
     }
     if sign {
-        index -= 1;
         buf[index] = '-' as u8;
+        index -= 1;
     }
     unsafe {
-        LCD5110_write_bytes(buf[index..].as_ptr() as *const u8, (buf.len()  - index) as u32);
+        LCD5110_write_bytes(
+            buf[index + 1..].as_ptr() as *const u8,
+            (buf.len() - index - 1) as u32,
+        );
     }
 }
 
@@ -86,12 +87,12 @@ pub extern "C" fn rust_main() {
         let mut position = Position {
             x: 0.0,
             y: 0.0,
-            theta: 0.0
+            theta: 0.0,
         };
         let mut odo_computer = OdometryComputer {
             position: &mut position,
-            oldTachoL: 0,
-            oldTachoR: 0,
+            old_left_ticks: 0,
+            old_right_ticks: 0,
         };
 
         loop {
@@ -104,17 +105,20 @@ pub extern "C" fn rust_main() {
             LCD5110_clear();
             display_text(0, 0, "This is RUST!");
 
-            output_data_line(1, "B: ", || brightness);
-            output_data_line(2, "Rng: ", || radar_range());
-            output_data_line(3, "Left : ", || left_ticks() );
-            output_data_line(4, "Right: ", || right_ticks());
+            let left_ticks = left_ticks(); //todo fix types
+            let right_ticks = right_ticks(); //todo fix types
+
+            output_data_line(1, "B:     ", || brightness);
+            output_data_line(2, "Rng:   ", || radar_range());
+            output_data_line(3, "Left:  ", || left_ticks);
+            output_data_line(4, "Right: ", || right_ticks);
 
             LCD5110_set_XY(3, 5);
             LCD5110_write_char(alarm_char(0));
             LCD5110_write_char(alarm_char(1));
             LCD5110_write_char(alarm_char(2));
             LCD5110_write_char(alarm_char(3));
-            odo_computer.compute_odometry(left_ticks(), right_ticks());
+            odo_computer.update(left_ticks, right_ticks);
 
             // TODO: f64 printing
             // output_data_line(4, "x:     ", || position.x);
@@ -123,61 +127,6 @@ pub extern "C" fn rust_main() {
             /*
                         void debug_output(const unsigned char *p, unsigned int len); //todo implement
             */
-        }
-    }
-}
-
-struct Position {
-    x: f64,
-    y: f64,
-    theta: f64,
-}
-
-struct OdometryComputer<'a> {
-    position: &'a mut Position,
-    oldTachoL: i32,
-    oldTachoR: i32,
-}
-
-fn sin(x: f64) -> f64 {
-    //    x - x*x*x / 6.0 + x*x*x*x*x / 120.0
-    unsafe { sinf64(x) }
-}
-
-fn cos(x: f64) -> f64 {
-    //    1.0 - x*x / 2.0 + x*x*x*x / 24.0
-    unsafe { cosf64(x) }
-}
-
-impl OdometryComputer<'_> {
-    fn compute_odometry(&mut self, tachoL: i32, tachoR: i32) {
-        let dL = tachoL - self.oldTachoL;
-        self.oldTachoL = tachoL;
-        let dR = tachoR - self.oldTachoR;
-        self.oldTachoR = tachoR;
-        let dTrackL: f64 = dL as f64 * PI / 180.0 * WHEEL_RADIUS_CM;
-        let dTrackR = dR as f64 * PI / 180.0 * WHEEL_RADIUS_CM;
-        let dTrack = dTrackR - dTrackL;
-        let dTrackAvr = (dTrackR + dTrackL) / 2.0;
-        let dTurnAngle = dTrack / WHEEL_BASE_CM;
-        let turnRadius = dTrackAvr / dTurnAngle;
-        let dx;
-        let dy;
-        if turnRadius.is_infinite() || turnRadius.is_nan() {
-            dx = dTrackAvr * cos(self.position.theta);
-            dy = dTrackAvr * sin(self.position.theta);
-        } else {
-            let turnAngle = self.position.theta - PI / 2.0;
-            dx = turnRadius * (cos(turnAngle + dTurnAngle) - cos(turnAngle));
-            dy = turnRadius * (sin(turnAngle + dTurnAngle) - sin(turnAngle));
-        }
-        self.position.x += dx;
-        self.position.y += dy;
-        self.position.theta += dTurnAngle;
-        if self.position.theta < 0.0 {
-            self.position.theta += 2.0 * PI;
-        } else if self.position.theta > 2.0 * PI {
-            self.position.theta -= 2.0 * PI;
         }
     }
 }
