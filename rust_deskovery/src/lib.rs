3a86016b80de85657e708d89bf3a4cc03b5e99d1
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2019-09-20 10:56:04 +0300

    Better motors control, screens handling

--- rust_deskovery/src/lib.rs
+++ rust_deskovery/src/lib.rs
@@ -10,16 +10,16 @@ use wifi::{Port, PortResult, Device};
 use data::{DeskoveryData, ServerData};
 
 
-use odometry::{OdometryComputer, Position};
+use odometry::OdometryComputer;
 use core::f64::consts::PI;
-use compat::{display_text_xy, debug_print, display_text, PRX_BR, PRX_BL, PRX_FR, PRX_FL, robot_idle};
+use compat::{display_text_xy, display_text, PRX_BR, PRX_BL, PRX_FR, PRX_FL, robot_idle};
 use compat::{
     delay_ms, display_bg_control, led_control, left_ticks, prxData, radar_range, right_ticks,
     LCD5110_clear, LCD5110_set_XY, LCD5110_write_char, LCD5110_write_pict, deskovery_motor,
     uart_output, uart_input,
 };
 use crate::compat::{debug_output, system_ticks};
-use crate::generated_images::RUST_LOGO_BYTES; //todo make safe
+use crate::generated_images::{RUST_LOGO_BYTES, CLION_LOGO_NORM_BYTES, CLION_LOGO_BYTES}; //todo make safe
 
 fn output_data_line<F>(x: u8, y: u8, label: &str, dataGetter: F)
     where
@@ -62,7 +62,7 @@ fn alarm_char(alarm_idx: u32) -> u8 {
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
-    let mut odo_computer = OdometryComputer::new();
+    let odo_computer = OdometryComputer::new();
     let port = RobotBrains {
         odo_computer,
         deskovery_data: [Default::default(); 10],
@@ -71,6 +71,7 @@ pub extern "C" fn rust_main() {
         left_motor: 0,
         right_motor: 0,
         sample_timestamp: 0,
+        screen_draw: RobotBrains::data_screen_draw,
     };
     let mut device = Device::new(port);
     unsafe {
@@ -94,13 +95,18 @@ pub extern "C" fn rust_main() {
         unsafe { led_control(true); }
         device.brains.server_data = device.make_post_request(&arr[0..device.brains.data_q_len],
 //                                                             "185.135.234.139", 8000).ok();
-                                                             "104.236.228.23", 8000).ok();
+                                                             "104.236.228.23", 8002).ok();
         unsafe { led_control(false); }
         device.brains.data_q_len = 0;
         device.brains.robot_loop();
     }
 }
 
+fn adjust_motor(josticAxis: i32) -> i32 {
+    let v = josticAxis.abs();
+    if v < 50 { return 0; }
+    return -josticAxis.signum() * (200 + (v - 50) * 800 / 1000);
+}
 
 pub struct RobotBrains {
     odo_computer: OdometryComputer,
@@ -110,6 +116,7 @@ pub struct RobotBrains {
     left_motor: i32,
     right_motor: i32,
     sample_timestamp: u32,
+    screen_draw: fn(&Self),
 }
 
 impl RobotBrains {
@@ -118,42 +125,71 @@ impl RobotBrains {
         match self.server_data {
             Some(data) => {
                 //tractor control
-                self.left_motor = -data.x;
-                self.right_motor = -data.y;
+                self.left_motor = adjust_motor(data.x);
+                self.right_motor = adjust_motor(data.y);
                 //auto control
 //                self.left_motor = -data.y / 2 + data.x / 2;
 //                self.right_motor = -data.y / 2 - data.x / 2;
                 unsafe { deskovery_motor(self.left_motor, self.right_motor, false); }
+                if data.b1 {
+                    self.screen_draw = Self::data_screen_draw;
+                } else if data.b2 {
+                    self.screen_draw = Self::clion_screen_draw;
+                } else if data.b3 {
+                    self.screen_draw = Self::rust_screen_draw;
+                } else if data.b4 {
+                    self.screen_draw = Self::clion_neg_screen_draw;
+                }
             }
             None => {}
         }
-        unsafe { LCD5110_clear(); }
-        output_data_line(0, 0, "LM: ", || self.left_motor);
-        output_data_line(0, 1, "RM: ", || self.right_motor);
+        (self.screen_draw)(self);
         unsafe {
+            self.odo_computer.update(left_ticks(), right_ticks());
+            if (system_ticks() - self.sample_timestamp) > 250 {
+                if self.data_q_len < self.deskovery_data.len() {
+                    self.deskovery_data[self.data_q_len] = DeskoveryData {
+                        x: self.odo_computer.position().x as i32,
+                        y: self.odo_computer.position().y as i32,
+                        th: (self.odo_computer.position().theta * 180.0 / PI) as i32,
+                        ps1: prxData.alarms[0],
+                        ps2: prxData.alarms[1],
+                        ps3: prxData.alarms[2],
+                        ps4: prxData.alarms[3],
+                        dto: radar_range(),
+                    };
+                    self.data_q_len += 1;
+                }
+                self.sample_timestamp = system_ticks();
+            }
+        }
+    }
+    pub fn rust_screen_draw(&self) {
+        unsafe {
+            LCD5110_write_pict(RUST_LOGO_BYTES.as_ptr());
+        }
+    }
+    pub fn clion_screen_draw(&self) {
+        unsafe {
+            LCD5110_write_pict(CLION_LOGO_NORM_BYTES.as_ptr());
+        }
+    }
+    pub fn clion_neg_screen_draw(&self) {
+        unsafe {
+            LCD5110_write_pict(CLION_LOGO_BYTES.as_ptr());
+        }
+    }
+    pub fn data_screen_draw(&self) {
+        unsafe {
+            LCD5110_clear();
+            output_data_line(0, 0, "LM: ", || self.left_motor);
+            output_data_line(0, 1, "RM: ", || self.right_motor);
             LCD5110_set_XY(12, 0);
             LCD5110_write_char(alarm_char(PRX_BR));
             LCD5110_write_char(alarm_char(PRX_BL));
             LCD5110_set_XY(12, 1);
             LCD5110_write_char(alarm_char(PRX_FR));
             LCD5110_write_char(alarm_char(PRX_FL));
-            self.odo_computer.update(left_ticks(), right_ticks());
-            if (system_ticks() - self.sample_timestamp) >250 {
-                if(self.data_q_len < self.deskovery_data.len()) {
-                    self.deskovery_data[self.data_q_len] = DeskoveryData{
-                        x: self.odo_computer.position().x as i32,
-                        y: self.odo_computer.position().y as i32,
-                        th: (self.odo_computer.position().theta *180.0/PI) as i32,
-                        ps1: prxData.alarms[0],
-                        ps2: prxData.alarms[1],
-                        ps3: prxData.alarms[2],
-                        ps4: prxData.alarms[3],
-                        dto: radar_range()
-                    };
-                    self.data_q_len +=1;
-                }
-                self.sample_timestamp = system_ticks();
-            }
         }
     }
 }
