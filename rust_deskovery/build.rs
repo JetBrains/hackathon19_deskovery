f505ad96e1c61ed2bc57892a52abd5f289d669f4
Kirill Bulatov <mail4score@gmail.com>
2019-10-20 14:58:49 +0300

    Remove the hardcoded includes requirement

--- rust_deskovery/build.rs
+++ rust_deskovery/build.rs
@@ -4,17 +4,10 @@ use std::path::{Path, PathBuf};
 fn generate_rust_bindings<P: AsRef<Path>>(out_path: P) {
     let result_path = out_path.as_ref().join("descovery_bindings.rs");
 
-    let includes = [
-        "/usr/local/lib/gcc/arm-none-eabi/7.3.1/include",
-        "C:\\tools\\GNU Tools Arm Embedded\\7 2018-q2-update\\lib\\gcc\\arm-none-eabi\\7.3.1\\include",
-        "/usr/local/Cellar/arm-none-eabi-gcc/8-2018-q4-major/gcc/lib/gcc/arm-none-eabi/8.2.1/include"
-    ];
-
     let builder = bindgen::Builder::default()
         .header("../Core/Inc/rust_header.h")
         .use_core()
         .ctypes_prefix("crate::compat::libc")
-        .clang_args(includes.iter().map(|include| format!("-I{}", include)))
         .clang_arg("--target=thumbv7em-none-eabi")
         .clang_arg("--verbose")
         .clang_arg("-nostdinc");
